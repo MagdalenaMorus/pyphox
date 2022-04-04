d89f3b4a6ff9c7f7673f18845e1cda2c98e3dca8
MagdalenaMorus <magdalenamor00@gmail.com>
2022-04-04 12:25:44 +0200

    Add initial code

---
+++ acc.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ACC_PATH = 'Raw Data.csv'
+df = pd.read_csv(ACC_PATH)
+df = df[df.columns[1]]
+print(df)
+df.plot()
+plt.title('acceleration x in time')
+plt.grid(True)
+plt.show()
