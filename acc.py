b47ec31e04cacb560f815539f96eb746a89efc84
piotr bosowski <piotr.bosowski@gmail.com>
2022-04-11 13:13:30 +0200

    Add data loading

--- acc.py
+++ acc.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+ACC_PATH = 'data/Raw Data.csv'
 
-def print_single_plot(column_name):
-    ACC_PATH = 'data/Raw Data.csv'
+
+def load_data(data_path, variables=('x', 'y', 'z')):
     df = pd.read_csv(ACC_PATH)
-    df = df[column_name]
-    print(df)
-    df.plot()
-    plt.title(f'{column_name} in time')
-    plt.grid(True)
+    columns = tuple(df[f'Linear Acceleration {var} (m/s^2)'].to_numpy()
+                    for var in variables)
+    return columns + (df['Time (s)'].to_numpy(),)
+
+
+def print_single_plot(column):
+    plt.plot(column)
+    # plt.title(f'{column_name} in time')
+    # plt.grid(True)
     plt.show()
 
-def print_all_plots():
-    print_single_plot('Linear Acceleration x (m/s^2)')
-    print_single_plot('Linear Acceleration y (m/s^2)')
-    print_single_plot('Linear Acceleration z (m/s^2)')
 
-print_all_plots()
-
-
+if __name__ == '__main__':
+    x, y, z, t = load_data(ACC_PATH)
+    print_single_plot(x)
+    dbg_stp = 5
