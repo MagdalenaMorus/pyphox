ed5d05802a7d2ab08cb41b39a7693dc4b80b4076
MagdalenaMorus <magdalenamor00@gmail.com>
2022-04-11 12:33:05 +0200

    add all three plots

--- acc.py
+++ acc.py
@@ -2,11 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-ACC_PATH = 'data/Raw Data.csv'
-df = pd.read_csv(ACC_PATH)
-df = df[df.columns[1]]
-print(df)
-df.plot()
-plt.title('acceleration x in time')
-plt.grid(True)
-plt.show()
+def print_single_plot(column_name):
+    ACC_PATH = 'data/Raw Data.csv'
+    df = pd.read_csv(ACC_PATH)
+    df = df[column_name]
+    print(df)
+    df.plot()
+    plt.title(f'{column_name} in time')
+    plt.grid(True)
+    plt.show()
+
+def print_all_plots():
+    print_single_plot('Linear Acceleration x (m/s^2)')
+    print_single_plot('Linear Acceleration y (m/s^2)')
+    print_single_plot('Linear Acceleration z (m/s^2)')
+
+print_all_plots()
+
+
